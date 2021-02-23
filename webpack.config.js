30aee938aa03350a2a6b2eea741caa2bef356aeb
gnkz <gnkz@users.noreply.github.com>
2021-02-23 14:16:27 -0300

    add _redirects file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -18,6 +19,7 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: 'src/index.html',
     }),
+    new CopyPlugin({ patterns: [{ from: '_redirects' }] }),
   ],
   devServer: {
     historyApiFallback: true,
